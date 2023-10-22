d5e9f8a57576345ea81d975876c1af2c5f7f50c7
Vidhisha Shetty <109327902+VidhishaS@users.noreply.github.com>
2023-10-23 01:42:57 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './Cards/Card';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Card  />
+      
+    </div>
+  );
+};
+
+export default App;
